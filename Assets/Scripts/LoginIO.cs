d54dc626842b3721ad852c16ebdf7df0cc2cd042
asef18766 <asef18766@gmail.com>
2020-02-08 16:32:29 +0800

    add gitignore

---
+++ Assets/Scripts/LoginIO.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+public class LoginIO : MonoBehaviour
+{
+	private SocketIOComponent socket;
+
+	public void Start() 
+	{
+		GameObject go = GameObject.Find("SocketIO");
+		socket = go.GetComponent<SocketIOComponent>();
+		
+		socket.On("open", OnOpen);
+		socket.On("error", OnError);
+		socket.On("close", OnClose);
+		socket.On("login" , OnLogined);
+	}
+
+	void Update()
+	{
+
+	}
+
+	# region Emit_Methods
+	public void Login(string name)
+	{
+		Dictionary<string , string> data = new Dictionary<string, string>();
+		data["playerName"] = name ;
+		Debug.Log("emit" + (new JSONObject(data)).ToString());
+		socket.Emit("login" , new JSONObject(data));
+	}
+	
+	# endregion
+	# region Listen_Events
+	public void OnOpen(SocketIOEvent e)
+	{
+		Debug.Log("O" + e);
+	}
+	
+	public void OnError(SocketIOEvent e)
+	{
+		Debug.Log("E" + e);
+	}
+	
+	public void OnClose(SocketIOEvent e)
+	{
+		Debug.Log("C" + e);
+	}
+	public void OnLogined(SocketIOEvent e)
+	{
+		Debug.Log("L" + e);
+	}
+	# endregion
+}
